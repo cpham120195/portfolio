9b77572c9b8b57154e6a3b7aedd3f94b276044b7
Chris Pham <Chris@Dennes-MacBook-Pro.local>
2021-08-11 16:11:37 -0700

    add styling to nav-bar

--- src/components/Button.js
+++ src/components/Button.js
@@ -14,7 +14,7 @@ export const Button = ({
     buttonSize}) => {
         const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
-        const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize: SIZEZ[0];
+        const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize: SIZES[0];
 
         return (
             <Link to='/about' className='btn-mobile'>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css'
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
     //set click to the opposite, switches to true and back to false if clicked
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, [])
+
+    window.addEventListener('resize', showButton);
+
+
+
     return (
        <>
         <nav className="navbar">
@@ -30,12 +51,16 @@ function Navbar() {
                     </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to="/contact" className='nav-links' onClick={closeMobileMenu}>Contact Me
+                    <Link to="/resume" className='nav-links' onClick={closeMobileMenu}>Resume
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to="/contact" className='nav-links' onClick={closeMobileMenu}>Contact
                     </Link>
                 </li>
 
             </ul>
-            {button && <Button buttonStyle='btn--outline'>About</Button>}
+            
 
             </div>
         </nav>
