08f5aaef884297bd042f91dd24f94ef2a18f01ad
Chris Pham <Chris@Dennes-MacBook-Pro.local>
2021-08-11 12:26:09 -0700

    add button component and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,32 +5,37 @@ function Navbar() {
     const [click, setClick] = useState(false);
     //set click to the opposite, switches to true and back to false if clicked
     const handleClick = () => setClick(!click);
-    const closeMenu = () => setClick(false);
+    const closeMobileMenu = () => setClick(false);
     return (
        <>
         <nav className="navbar">
             <div className="navbar-container">
             <Link to="/" className="navbar-logo">
-            <i className="fas fa-laptop-code" />
+            CP Codes <i className="fas fa-laptop-code" />
             </Link>
             <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
             <ul className={click ? 'nav-menu-active ' : 'nav-menu'}>
                 <li className='nav-item'>
-                    <Link to="/" className='nav-links' onClick={closeMenu}>About Me</Link>
+                    <Link to="/about" className='nav-links' onClick={closeMobileMenu}>About
+                    </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to="/" className='nav-links' onClick={closeMenu}>Skills</Link>
+                    <Link to="/skills" className='nav-links' onClick={closeMobileMenu}>Skills
+                    </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to="/" className='nav-links' onClick={closeMenu}>Projects</Link>
+                    <Link to="/projects" className='nav-links' onClick={closeMobileMenu}>Projects
+                    </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to="/" className='nav-links' onClick={closeMenu}>Contact Me</Link>
+                    <Link to="/contact" className='nav-links' onClick={closeMobileMenu}>Contact Me
+                    </Link>
                 </li>
 
             </ul>
+            {button && <Button buttonStyle='btn--outline'>About</Button>}
 
             </div>
         </nav>
